c33e32861c7af083001db28d407296f17113f2ca
jahanara khatun <zarakhatun58@gmail.com>
2021-10-11 18:53:07 +0530

    finished

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
+        <div className="nav-bg">
             <NavBar></NavBar>
         </div>
     );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,28 +14,28 @@ const NavBar = () => {
     }
     return (
         <div>
-            <Navbar className="bg-nav" expand="lg" variant="dark">
+            <Navbar expand="lg" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/home">Our Code Education</Navbar.Brand>
+                    <Navbar.Brand href="/home" className="education-logo">Our Code Education</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link className="nav-links" href="/home">
-                            <NavLink style={style} activeStyle={activeStyle}  to="/home">Home</NavLink>
-                        </Nav.Link>
-                        <Nav.Link className="nav-links" href="/services">
-                            <NavLink style={style} activeStyle={activeStyle} to="/services">Services</NavLink>
-                        </Nav.Link>
-                        <Nav.Link className="nav-links" href="/about">
-                            <NavLink style={style} activeStyle={activeStyle} to="/about">About</NavLink>
-                        </Nav.Link>
-                        <Nav.Link className="nav-links" href="/contact">
-                            <NavLink style={style} activeStyle={activeStyle} to="/contact">Contact</NavLink>
-                        </Nav.Link>
-                    </Nav>
+                        <Nav className="ms-auto">
+                            <Nav.Link className="nav-links" href="/home">
+                                <NavLink style={style} activeStyle={activeStyle} to="/home">Home</NavLink>
+                            </Nav.Link>
+                            <Nav.Link className="nav-links" href="/services">
+                                <NavLink style={style} activeStyle={activeStyle} to="/services">Services</NavLink>
+                            </Nav.Link>
+                            <Nav.Link className="nav-links" href="/about">
+                                <NavLink style={style} activeStyle={activeStyle} to="/about">About</NavLink>
+                            </Nav.Link>
+                            <Nav.Link className="nav-links" href="/contact">
+                                <NavLink style={style} activeStyle={activeStyle} to="/contact">Contact</NavLink>
+                            </Nav.Link>
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
-                </Navbar>
+            </Navbar>
         </div>
     );
 };
